ffac9db9b89db023ca6dbcff865b9539575246dd
Sam Wells <sfwells0518@gmail.com>
2023-04-25 12:11:27 -0500

    add index, show, new, create and destroy actions and their associated view html.erb files

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,32 @@
 class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+    render :index 
+  end
+
+  def show
+    @pet = Pet.find_by(id: params[:id])
+    render :show 
+  end 
+
+  def new
+    @pet = Pet.new
+    render :new 
+  end
+
+  def create
+    @pet = Pet.create(
+      age: params[:pet][:age],
+      breed: params[:pet][:breed],
+      color: params[:pet][:color],
+      image: params[:pet][:image],
+    )
+    redirect_to "/pets"
+  end
+
+  def destroy
+    @pet = Pet.find_by(id: params[:id])
+    @pet.destroy 
+    redirect_to "/pets", status: :see_other 
+  end
 end
