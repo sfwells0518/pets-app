a9b52acbd673f99564097fac110fcc9d42222bd8
Sam Wells <sfwells0518@gmail.com>
2023-04-25 13:59:05 -0500

    generate migration to add user_id to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_181247) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_185801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_181247) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "name"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
