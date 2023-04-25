5780e3886e64a5b0bb71112b48a41a40db91c490
Sam Wells <sfwells0518@gmail.com>
2023-04-25 13:43:45 -0500

    add sessions routes, auth for current user in applications controller and a new.html.erb file for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :pets 
 
+  # Sign Up Routes:
   get "/signup" => "users#new"
   post "/users" => "users#create"
+
+  # Login Routes:
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
+
 end
